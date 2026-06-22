f359c2029123c02925726b6d3cb02c9a0305d150
omesh <engineeromesh@gmail.com>
2026-06-22 20:06:08 +0530

    Third commit

--- app.js
+++ app.js
@@ -33,8 +33,8 @@ main().then(()=>
     console.log("Connected to DB")).catch(err => console.log(err));
 
 async function main() {
-await mongoose.connect('mongodb://127.0.0.1:27017/omnilust');
-//   await mongoose.connect(dbURL);
+// await mongoose.connect('mongodb://127.0.0.1:27017/omnilust');
+  await mongoose.connect(dbURL);
 }
 
 app.listen(8080,()=>{
